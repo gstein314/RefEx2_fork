492d754c6a1a1d4a9728b2232a18326424aac32d
大江竜太 <oe@penqe.com>
2021-01-12 10:48:29 +0900

    medianの数値でソート

--- pages/_organism/_project/_gid.vue
+++ pages/_organism/_project/_gid.vue
@@ -17,15 +17,15 @@
       </thead>
       <tbody>
         <tr v-for="result in results.r_inf" :key="result.ncbiGeneId">
-          <td class="sample_description">{{ result['Description'] }}</td>
-          <td class="median">{{ result['Median'] }}</td>
-          <td class="sample_type">{{ result['Sample types category'] }}</td>
-          <td class="experiment">{{ result['Experiments category'] }}</td>
-          <td class="cl">{{ result['CL label'] }}</td>
-          <td class="sex">{{ result['Sex'] }}</td>
-          <td class="age">{{ result['Age'] }}</td>
-          <td class="stage">{{ result['Developmental stage'] }}</td>
-          <td class="ncit">{{ result['NCIT label'] }}</td>
+          <td class="sample_description">{{ result["Description"] }}</td>
+          <td class="median">{{ result["Median"] }}</td>
+          <td class="sample_type">{{ result["Sample types category"] }}</td>
+          <td class="experiment">{{ result["Experiments category"] }}</td>
+          <td class="cl">{{ result["CL label"] }}</td>
+          <td class="sex">{{ result["Sex"] }}</td>
+          <td class="age">{{ result["Age"] }}</td>
+          <td class="stage">{{ result["Developmental stage"] }}</td>
+          <td class="ncit">{{ result["NCIT label"] }}</td>
         </tr>
       </tbody>
     </table>
@@ -33,23 +33,45 @@
 </template>
 
 <script>
-import $ from 'jquery'
-import * as d3 from "d3"
-import axios from 'axios'
+import $ from "jquery";
+import * as d3 from "d3";
+import axios from "axios";
 
 export default {
-  async asyncData ({ params, error, payload }) {
-    let data = await axios.get(`http://refex2-api.bhx.jp/api/dist/${params.gid}`)
-    console.log(data.data)
+  async asyncData({ params, error, payload }) {
+    let data = await axios.get(
+      `http://refex2-api.bhx.jp/api/dist/${params.gid}`
+    );
+    console.log(data);
+    // ここにソート機能を実装する！
+    console.log(data.data);
+    console.log(data.data.r_inf);
+
+    data.data.r_inf.sort(function(a,b) {
+      return b.Median - a.Median;
+    });
+
+    // sort練習用
+    // var items = [
+    //   { name: "Edward", value: 21 },
+    //   { name: "Sharpe", value: 37 },
+    //   { name: "And", value: 45 },
+    //   { name: "The", value: -12 },
+    //   { name: "Magnetic", value: 13 },
+    //   { name: "Zeros", value: 37 },
+    // ];
+    // items.sort(function (a, b) {
+    //   return a.value - b.value;
+    // });
+    // console.log(items);
+
+    // ここまでソート機能を実装する
     return {
-      results: data.data
-    }
-
+      results: data.data,
+    };
   },
-  mounted() {
-
-  }
-}
+  mounted() {},
+};
 </script>
 
 <style lang="sass">
