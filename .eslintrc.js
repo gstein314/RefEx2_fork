a613ae788f0b5c4b2a5bc7dd5bedd61ea4092928
MarleenEliza <marleen.dijkman@outlook.com>
2022-02-09 19:39:06 +0900

    update packages

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended',
+    'prettier',
+  ],
+  plugins: ['import', 'vue', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        semi: true,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        vueIndentScriptAndStyle: true,
+      },
+    ],
+  },
+};
