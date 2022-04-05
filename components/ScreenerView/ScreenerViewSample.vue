a1645e70b2c38be8c5d381d13eb1ba07dfaeebc9
MarleenEliza <marleen.dijkman@outlook.com>
2022-04-05 09:55:54 +0900

    turn sample screener dynamic

--- components/ScreenerView/ScreenerViewSample.vue
+++ components/ScreenerView/ScreenerViewSample.vue
@@ -2,138 +2,59 @@
   <!-- v-html setup neccesary for plugin, does NOT use user input/API data and is therefore safe to use -->
   <!-- eslint-disable vue/no-v-html -->
   <div>
-    <h3>
-      Genes that are specifically expressed in a given sample by classification
+    <h3 v-if="screenerInfo.description !== ''">
+      {{ screenerInfo.description }}
     </h3>
-    <div class="classification_wrapper">
-      <h4>
-        Sample types <span class="tag">FANTOM5</span>
-        <span class="example"
-          >e.g.
-          <span
-            class="sample_value"
-            @click="updateParameter('project', 'cell lines')"
-            >cell lines</span
-          >,
-          <span
-            class="sample_value"
-            @click="updateParameter('project', 'stem cells')"
-            >stem cells</span
-          >,
-          <span
-            class="sample_value"
-            @click="updateParameter('project', 'primary cells')"
-            >primary cells</span
-          >,
-          <span
-            class="sample_value"
-            @click="updateParameter('project', 'tissues')"
-            >tissues</span
-          >
-        </span>
-      </h4>
-      <vue-simple-suggest
-        v-model="parameters.project"
-        :filter-by-query="true"
-        :list="autoComplete.project"
-        :max-suggestions="100"
-        class="text_search_sample_types"
-        placeholder="cell lines"
-        @input="updateParameter"
+    <div :class="{ classification_wrapper: classificationItems.length > 1 }">
+      <div
+        v-for="item of classificationItems"
+        :key="item.column"
+        class="classification_item"
       >
-        <div
-          slot="suggestion-item"
-          slot-scope="{ suggestion }"
-          v-html="$boldenSuggestion(suggestion, parameters.project)"
-        ></div>
-      </vue-simple-suggest>
-      <h4>
-        Cell types <span class="tag">Cell Ontology</span>
-        <span class="example"
-          >e.g.
-          <span
-            class="sample_value"
-            @click="updateParameter('CL', 'hepatocyte')"
-            >hepatocyte</span
-          >
-        </span>
-      </h4>
-      <vue-simple-suggest
-        v-model="parameters.CL"
-        :filter-by-query="true"
-        :list="autoComplete.CL"
-        :max-suggestions="100"
-        class="text_search_cell_types"
-        placeholder="CD14"
-        @input="$emit('updateParameters', parameters)"
-      >
-        <div
-          slot="suggestion-item"
-          slot-scope="{ suggestion }"
-          v-html="$boldenSuggestion(suggestion, parameters.CL)"
-        ></div>
-      </vue-simple-suggest>
-      <h4>
-        Anatomical structures <span class="tag">UBERON</span>
-        <span class="example"
-          >e.g.
-          <span class="sample_value" @click="updateParameter('UBERON', 'liver')"
-            >liver</span
-          >
-        </span>
-      </h4>
-      <vue-simple-suggest
-        v-model="parameters.UBERON"
-        :filter-by-query="true"
-        :list="autoComplete.UBERON"
-        :max-suggestions="100"
-        class="text_search_anatomical_structures"
-        placeholder="skin"
-        @input="$emit('updateParameters', parameters)"
-      >
-        <div
-          slot="suggestion-item"
-          slot-scope="{ suggestion }"
-          v-html="$boldenSuggestion(suggestion, parameters.UBERON)"
-        ></div>
-      </vue-simple-suggest>
-      <h4>
-        Biomedical concepts <span class="tag"> NCI Thesaurus (NCIt)</span>
-        <span class="example"
-          >e.g.
-          <span
-            class="sample_value"
-            @click="updateParameter('NCIT', 'Osteosarcoma')"
-            >Osteosarcoma</span
-          >,
-          <span
-            class="sample_value"
-            @click="updateParameter('NCIT', 'Ovarian Carcinoma')"
-            >Ovarian Carcinoma</span
-          >
-        </span>
-      </h4>
-      <vue-simple-suggest
-        v-model="parameters.NCIT"
-        :filter-by-query="true"
-        :list="autoComplete.NCIT"
-        :max-suggestions="100"
-        class="text_search_biomedical_concepts"
-        placeholder="leukemia"
-        @input="$emit('updateParameters', parameters)"
-      >
-        <div
-          slot="suggestion-item"
-          slot-scope="{ suggestion }"
-          v-html="$boldenSuggestion(suggestion, parameters.NCIT)"
-        ></div>
-      </vue-simple-suggest>
+        <h4>
+          {{ item.label }}
+          <span v-if="item.note" class="tag">{{ item.note }}</span>
+          <span class="example">
+            e.g.
+            <template v-for="(sample, sampleIndex) of item.examples">
+              <span
+                :key="`${sampleIndex}_sample`"
+                class="sample_value"
+                @click="updateParameter(item.column, sample.route)"
+              >
+                {{ sample.label }}
+              </span>
+              <span
+                v-if="sampleIndex < item.examples.length - 1"
+                :key="`${sampleIndex}_separator`"
+                >,</span
+              >
+            </template>
+          </span>
+        </h4>
+        <vue-simple-suggest
+          v-model="parameters[item.column]"
+          :filter-by-query="true"
+          :list="autoComplete[item.column]"
+          :max-suggestions="100"
+          class="text_search_sample_types"
+          :placeholder="item.placeholder"
+          @input="updateParameter"
+        >
+          <div
+            slot="suggestion-item"
+            slot-scope="{ suggestion }"
+            v-html="$boldenSuggestion(suggestion, parameters[item.column])"
+          ></div>
+        </vue-simple-suggest>
+      </div>
     </div>
   </div>
 </template>
 <script>
   /* eslint-disable vue/prop-name-casing */
   import VueSimpleSuggest from 'vue-simple-suggest';
+  import { mapGetters } from 'vuex';
 
   export default {
     components: {
@@ -141,25 +62,30 @@
     },
     data() {
       return {
-        // passed down to API
-        parameters: {
-          project: '',
-          CL: '',
-          NCIT: '',
-          UBERON: '',
-        },
+        // passed down to API. will hold keys of the column parameter of screener > classification_items > column of filters.json
+        parameters: {},
+        // will contain same keys as parameters. Autocompletion that does not come from the API should be hardcoded here in advance
         autoComplete: {
-          project: ['cell lines', 'stem cells', 'primary cells', 'tissues'],
-          CL: [],
-          NCIT: [],
-          UBERON: [],
+          Description: ['cell lines', 'stem cells', 'primary cells', 'tissues'],
         },
-        debounce: null,
       };
     },
-    created() {
-      Object.keys(this.autoComplete).forEach(key => {
-        if (this.autoComplete[key].length > 1) return;
+    computed: {
+      ...mapGetters({
+        activeFilter: 'active_filter',
+      }),
+      screenerInfo() {
+        return this.activeFilter.screener;
+      },
+      classificationItems() {
+        return this.screenerInfo.classification_items;
+      },
+    },
+    async created() {
+      this.classificationItems.forEach(item => {
+        const key = item.column;
+        this.$set(this.parameters, key, '');
+        if (key in this.autoComplete) return;
         this.$axios
           .$get(`api/vocablary?annotation=${key.toUpperCase()}%20label`)
           .then(data => {
