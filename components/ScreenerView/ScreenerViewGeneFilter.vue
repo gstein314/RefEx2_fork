2a53c4fe1e3ad7929c57656e1f0c59a7fed3ec5d
maruk0chan <marco.chan@penqe.com>
2023-03-01 10:56:30 +0900

    refactor and del unused code

--- components/ScreenerView/ScreenerViewGeneFilter.vue
+++ components/ScreenerView/ScreenerViewGeneFilter.vue
@@ -191,36 +191,23 @@
       ...mapGetters({
         activeDataset: 'active_dataset',
       }),
-      isSelectedArray() {
+      isCheckedSelectedArray() {
         const list = this.screenerFilter.list;
         const filteredList = list.filter(({ isChecked }) => isChecked);
         const defaultItem = this.screenerFilter.defaultItem;
         const isDefaultItem = item => _.isEqual(item, defaultItem);
-        const isSelectedArray = list
+        const isCheckedSelectedArray = list
           .filter(({ isChecked }) => isChecked)
           .map(({ isSelected }) => isSelected);
-        console.log(
-          '🚀 > isSelectedArray > isSelectedArray before:',
-          isSelectedArray
-        );
-
-        if (list.length === 1 && !list[0].isChecked) {
-          list[0].isChecked = isDefaultItem(list[0]);
-        } else {
-          for (const [i, item] of filteredList.entries()) {
-            if (isDefaultItem(item) || !item.isChecked) {
-              isSelectedArray[i] = true;
-            }
+        for (const [i, item] of filteredList.entries()) {
+          if (isDefaultItem(item) || !item.isChecked) {
+            isCheckedSelectedArray[i] = true;
           }
         }
-        console.log(
-          '🚀 > isSelectedArray > isSelectedArray after:',
-          isSelectedArray
-        );
-        return isSelectedArray;
+        return isCheckedSelectedArray;
       },
       isAllSampleSelected() {
-        return this.isSelectedArray.every(Boolean);
+        return this.isCheckedSelectedArray.every(Boolean);
       },
       groupOptions() {
         const target = this.activeDataset.dataset;
@@ -243,16 +230,6 @@
           return true;
         }
         return false;
-      },
-    },
-    watch: {
-      isSelectedArray: {
-        handler(newValue, oldValue) {
-          // Note: `newValue` will be equal to `oldValue` here
-          // on nested mutations as long as the object itself
-          // hasn't been replaced.
-        },
-        deep: true,
       },
     },
     mounted() {
