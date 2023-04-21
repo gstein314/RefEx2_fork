4c4e015b123edb1ff6008f896a1c2134614ebdbc
maruk0chan <marco.chan@penqe.com>
2023-04-21 15:22:13 +0900

    change component structure and document

--- components/icons/IconBase.vue
+++ components/icons/IconBase.vue
@@ -1,37 +1,16 @@
 <template>
-  <div
-    class="svg_wrapper"
-    :style="{
-      'min-width': `${wrapperSize}px`,
-      height: `${wrapperSize}px`,
-    }"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      :width="size[0]"
-      :height="size[1]"
-      :viewBox="`0 0 ${size[0]} ${size[1]}`"
-      :aria-labelledby="iconName"
-      role="presentation"
-    >
-      <title :id="iconName" lang="en">{{ iconName }} icon</title>
-      <g :fill="iconColor">
-        <component :is="iconComponent" :stroke-color="iconColor" />
-      </g>
-    </svg>
+  <div class="svg_wrapper">
+    <img
+      :src="require(`~/assets/img/${iconName}.svg`)"
+      :style="{
+        width: `${wrapperSize}px`,
+        height: `${wrapperSize}px`,
+      }"
+      :alt="iconName"
+    />
   </div>
 </template>
 <script>
-  // svg size will be maintained at the same ratio and limit under svg_wrapper size
-  const sizeMap = new Map([
-    ['Human', [24, 62]],
-    ['Mouse', [63, 24]],
-    ['Arabidopsis thaliana', [82.29, 164.57]],
-  ]);
-
-  // wrapper is set to be square (1:1)
-  const wrapperSize = 60;
-
   export default {
     props: {
       iconName: {
@@ -44,41 +23,8 @@
       },
     },
     computed: {
-      size() {
-        const [originalWidth, originalHeight] = sizeMap.get(this.iconName) || [
-          wrapperSize,
-          wrapperSize,
-        ];
-        const ratio = originalWidth / originalHeight;
-        const isPerfectSized =
-          originalWidth === wrapperSize || originalHeight === wrapperSize;
-        const isOversized =
-          originalWidth > wrapperSize || originalHeight > wrapperSize;
-        const isUndersized =
-          originalWidth < wrapperSize && originalHeight < wrapperSize;
-        const [width, height] = isPerfectSized
-          ? [originalWidth, originalHeight]
-          : isOversized
-          ? originalWidth > originalHeight
-            ? [wrapperSize, wrapperSize / ratio]
-            : [wrapperSize * ratio, wrapperSize]
-          : isUndersized
-          ? originalWidth > originalHeight
-            ? [wrapperSize, wrapperSize / ratio]
-            : [wrapperSize * ratio, wrapperSize]
-          : [wrapperSize, wrapperSize];
-        return [width, height];
-      },
-
-      parsedName() {
-        return this.iconName
-          .split(' ')
-          .map(word => this.$firstLetterUppercase(word))
-          .join('');
-      },
-      iconComponent() {
-        import(`~/components/icons/Icon${this.parsedName}.vue`);
-        return () => import(`~/components/icons/Icon${this.parsedName}.vue`);
+      wrapperSize() {
+        return 60;
       },
     },
   };
