94bf6cc31637ffaec84d27826113937898bda01c
maruk0chan <marco.chan@penqe.com>
2023-04-21 13:01:18 +0900

    adjust svg size according to the wrapper

--- components/icons/IconBase.vue
+++ components/icons/IconBase.vue
@@ -1,19 +1,28 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    :width="size[0]"
-    :height="size[1]"
-    :viewBox="`0 0 ${size[0]} ${size[1]}`"
-    :aria-labelledby="iconName"
-    role="presentation"
+  <div
+    class="svg_wrapper"
+    :style="{
+      'min-width': `${wrapperSize}px`,
+      height: `${wrapperSize}px`,
+    }"
   >
-    <title :id="iconName" lang="en">{{ iconName }} icon</title>
-    <g :fill="iconColor">
-      <component :is="iconComponent" :stroke-color="iconColor" />
-    </g>
-  </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="size[0]"
+      :height="size[1]"
+      :viewBox="`0 0 ${size[0]} ${size[1]}`"
+      :aria-labelledby="iconName"
+      role="presentation"
+    >
+      <title :id="iconName" lang="en">{{ iconName }} icon</title>
+      <g :fill="iconColor">
+        <component :is="iconComponent" :stroke-color="iconColor" />
+      </g>
+    </svg>
+  </div>
 </template>
 <script>
+  // svg size will be maintained at the same ratio and limit under svg_wrapper size
   const sizeMap = new Map([
     ['Human', [24, 62]],
     ['Mouse', [63, 24]],
@@ -21,6 +30,9 @@
     ['oryza_sativa', [42, 71]],
     ['bombyx_mori', [43, 10]],
   ]);
+
+  // wrapper is set to be square (1:1)
+  const wrapperSize = 60;
 
   export default {
     props: {
@@ -35,7 +47,31 @@
     },
     computed: {
       size() {
-        return sizeMap.get(this.iconName) ?? [24, 62];
+        const originalSize = sizeMap.get(this.iconName) ?? [
+          wrapperSize,
+          wrapperSize,
+        ];
+        const [x, y] = originalSize;
+        const ratio = x / y;
+        const isOversized = x > wrapperSize || y > wrapperSize;
+        const isUndersized = x < wrapperSize || y < wrapperSize;
+        const xIsLonger = x > y;
+
+        if (isOversized) {
+          if (xIsLonger) {
+            return [wrapperSize, wrapperSize / ratio];
+          } else {
+            return [wrapperSize * ratio, wrapperSize];
+          }
+        }
+        if (isUndersized) {
+          if (xIsLonger) {
+            return [wrapperSize, wrapperSize / ratio];
+          } else {
+            return [wrapperSize * ratio, wrapperSize];
+          }
+        }
+        return [wrapperSize, wrapperSize];
       },
       parsedName() {
         return this.iconName
@@ -50,3 +86,11 @@
     },
   };
 </script>
+<style lang="sass" scoped>
+  .svg_wrapper
+    display: inline-block
+    text-align: center
+    // line-height: 1
+    width: 60px
+    height: 60px
+</style>
