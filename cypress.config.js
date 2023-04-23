774e4e985e4ea0d4d5102d29fb458dab10581f8f
maruk0chan <marco.chan@penqe.com>
2023-04-23 12:35:29 +0900

    add baseUrl for best practice

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,6 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:8484',
   },
 });
